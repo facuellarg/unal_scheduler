5c10a4a3cd55a6650937f9687aea61dd893572e3
Freddy Cuellar <facuellarg@unal.edu.co>
2020-08-22 18:36:00 -0500

    css

--- gui/sheduler/src/js/components/Panel/index.js
+++ gui/sheduler/src/js/components/Panel/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React ,{ useState, useEffect } from 'react';
 import Cell from './../Cell'
 import HeaderCell from './../HeaderCell'
 import './Panel.css'
 
+
+
+
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+
+const useWindowDimensions =()=> {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 const Panel =(props)=>{
-    const {horario} = props
-    const days =['lunes','martes','miercoles','jueves','viernes','sabado']
-    const get_hour_interval=(horario)=>{
+  const {horario} = props
+  const days =['lunes','martes','miercoles','jueves','viernes','sabado']
+  const { height, width } = useWindowDimensions();
+
+  const get_hour_interval=(horario)=>{
       let max = 0
       let min = 25 
       Object.keys(horario).map( code=>{
@@ -25,8 +55,12 @@ const Panel =(props)=>{
         return ""
       })
       return {'min':min,'max':max}
-    }
-    const gradient_time = (<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+  }
+
+
+
+
+  const gradient_time = (<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
                       <stop offset="0%" style={{stopColor:'rgb(100,100,100)',stopOpacity:.8}} />
                       <stop offset="100%" style={{stopColor:'rgb(180,180,180)',stopOpacity:.5}} />
                     </linearGradient>)
@@ -45,11 +79,12 @@ const Panel =(props)=>{
     return(
         <div className={'panel-container'} style={{gridTemplateRows: `repeat(${rows+1}, auto)`}}>
            <svg style={{height:'100%',width:"100%",gridRow:`1 / span ${rows+1}`, gridColumn:1,margin:0,padding:0}}>
-              {gradient_time}
               {gradient_day}
+              {gradient_time}
               <rect className={'rectangle'} width="100%" height="100%" style={{fill:`url(#${gradient_time.props['id']})`}}  />
           </svg>
           <HeaderCell day={'Hora'}  style={{gridColumn:1, gridRow:1}}></HeaderCell>
+          
           
           {days.map( (day,key)=>{
             return(
@@ -72,6 +107,13 @@ const Panel =(props)=>{
                                 gridColumn:1,
                                 }}>
                         </HeaderCell>
+          )
+          })}
+          {[...Array(rows).keys()].map( (val,key)=>{
+          return(
+            <svg key={key} style={{gridColumn:"1 / span 7", gridRow:`${val+2}`}} height="100%" width="100%">
+              <line x1="0" y1="100%" x2={width} y2="100%" style={{stroke:"rgba(219,219,219,0.9)",strokeWidth:2}} />
+          </svg>
           )
           })}
              
@@ -101,7 +143,11 @@ const Panel =(props)=>{
                 )
             )
         })
-          }</div>
+          }
+          <svg style={{marginBottom:"3%",gridColumn:"1 / span 7", gridRow:1}} height="100%" width="100%" >
+            <line x1="0" y1="100%" x2={width} y2="100%" style={{ stroke:"rgba(219,219,219,0.9)",strokeWidth:2}} />
+          </svg>
+          </div>
     )
 }
 
