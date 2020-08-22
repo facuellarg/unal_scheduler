86a44098bd6bc3214ecc743207e0a4dbc03c1609
Freddy Cuellar <facuellarg@unal.edu.co>
2020-08-22 04:05:38 -0500

    remove password

---
+++ gui/sheduler/src/js/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+// import 'resize-observer-polyfill/dist/ResizeObserver.global';
+const App=()=> {
+
+  return (<h1>HOLA</h1>)
+}
+
+export default App;
+
+
